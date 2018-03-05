25fe4e67a2c0517e13fa5e0b15cc2a178f633b88
Suzanne Richman <suzanne@favoredfortune.com>
2018-03-05 11:21:03 -0800

    updated and created view js files, routes and constructor js file

---
+++ scripts/models/routes.js
@@ -0,0 +1,7 @@
+'use strict';
+
+page('/', 'hello World');//homepage/request page
+// page('/results', 'hello World');//results page
+// page()'/history','hello world');//history page
+
+page();
---
+++ scripts/views/errorView.js
@@ -0,0 +1,15 @@
+'use strict';
+var app = app || {};
+
+(function (module) {
+  const errorView = {};
+  
+  errorView.initErrorPage = function () {
+    $('.container').hide();
+    $('.error-view').show();
+    $('#error-message').empty();
+    let template = Handlebars.compile($('#error-template').text());
+    $('#error-message').append(template(err));
+  };
+  module.errorView = errorView;
+})(app);
