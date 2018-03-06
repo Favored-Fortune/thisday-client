e012d4a2a3bdcd76683a9ddb4e516bd0c75d8b52
Kris Sakarias <ksakarias@gmail.com>
2018-03-05 16:51:24 -0800

    working on handling nyt data

--- scripts/models/news.js
+++ scripts/models/news.js
@@ -7,8 +7,15 @@ var API_URL = 'http://localhost:3000';
 
   News.all = [];
 
-  function News() {
-    
+  function News(articleObj) {
+    Object.keys(articleObj).forEach(key => this[key] = articleObj[key]);
+  }
+
+  News.loadAll = articles => {
+    let data = JSON.parse(articles.text);
+    console.log(data.response.docs)
+    News.all = data.response.docs.filter(object => object.pub_date === '1960-01-01T00:00:00Z' && object.print_page === '1')
+    // News.all = test => new News(test);
   }
 
   $('#requestDate').on('submit', function(event) {
@@ -17,8 +24,9 @@ var API_URL = 'http://localhost:3000';
     let month = event.target[1].value;
     let day = event.target[2].value;
 
-    $.get(`${API_URL}/nyt/articles`)
-      .then(results => console.log(results.text));
+    $.get(`${API_URL}/nyt/articles/${year}/${month}`)
+      .then(News.loadAll)
+      // .then(results => console.log(JSON.parse(results.text)));
   })
 
 
