34714d293b29732ef7f5fba59bd0595c02322e5f
Kris Sakarias <ksakarias@gmail.com>
2018-03-05 16:04:05 -0800

    getting data from nyt

--- scripts/models/news.js
+++ scripts/models/news.js
@@ -11,11 +11,16 @@ var API_URL = 'http://localhost:3000';
     
   }
 
-  News.fetchDay = () => {
-    $.get(`${API_URL}/nyt/articles`)
-      .then(console.log());
+  $('#requestDate').on('submit', function(event) {
+    event.preventDefault();
+    let year = event.target[0].value;
+    let month = event.target[1].value;
+    let day = event.target[2].value;
 
-  }
+    $.get(`${API_URL}/nyt/articles`)
+      .then(results => console.log(results.text));
+  })
+
 
   module.News = News;
 })(app);
---
+++ scripts/models/routes.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// page('/', ctx => app.thisdayView;
+//homepage/request page
+// page('/results', 'hello World');//results page
+// page()'/history','hello world');//history page
+
+// page();
