b1bd0a393da87250e9b639a25cde37f88551c16a
Steven Starwalt <steven.starwalt@yahoo.com>
2018-03-07 09:26:11 -0800

    failed to get the data from today in history, need ruby

--- scripts/views/requestView.js
+++ scripts/views/requestView.js
@@ -1,42 +1,45 @@
 'use strict';
 
 var app = app || {};
-
+var __API_URL__ = 'http://localhost:3000';
 (module => {
   const requestView = {};
 
-  User.all = [];
+  //   User.all = [];
 
-  function User (user) {
-    this.username = username,
-    this.dateSearched = fav_date
-  }
+  //   function User (user) {
+  //     this.username = username,
+  //     this.dateSearched = fav_date
+  //   }
 
-  User.checkUser = function(ctx) {
-    console.log(ctx);
-    User.getAll()
-      .then(User.all.includes(ctx.username) ? resultsView.initKnownPage : resultsView.initViewPage)
-      .catch(errorCallback);
-    new User(ctx);
-    User.all.push(this);
-  };
+  //   User.checkUser = function(ctx) {
+  //     console.log(ctx);
+  //     User.getAll()
+  //       .then(User.all.includes(ctx.username) ? resultsView.initKnownPage : resultsView.initViewPage)
+  //       .catch(errorCallback);
+  //     new User(ctx);
+  //     User.all.push(this);
+  //   };
 
-  User.loadAll = rows => User.all = rows.map(user => new User(user));
+  //   User.loadAll = rows => User.all = rows.map(user => new User(user));
 
-  // User.getAll = callback =>
-  //   $.get(`${__API_URL__}/api/v1/users`)
-  //     .then(User.loadAll)
+  requestView.getAll = () =>
+    $.get('https://history.muffinlabs.com/date/12/20')
+      .then(console.log)
   //     .catch(errorCallback);
   // const resultView = {};
 
   // let month = 10;
   // let day = 25;
 
-  $('#submit-user').click(() => {
-    app.jsonP.get(`https://history.muffinlabs.com/10/25`, function(data){
-      console.log(data);
-    });
-  });
+  // $( "#submit-user" ).on( "click", function() {
+  //   console.log( $( this ).text() );
+  // });
+  // $.get(`https://history.muffinlabs.com/10/25`, (request, response)){
+
+  //   console.log(data);
+  // }
+  // });
   // requestView.getAll = () => {
   //   $.get(`https://history.muffinlabs.com/${month}/${day}`)
   //   console.log('history');
@@ -48,119 +51,119 @@ var app = app || {};
 
   // }
 
-  
 
-  
-  
+
+
+
   module.requestView = requestView;
 
 })(app);
 
-(module => {
+// (module => {
 
-  const historyData = {
-    host: 'https://history.muffinlabs.com/',
+//   const historyData = {
+//     host: 'https://history.muffinlabs.com/',
 
-  
-    //  Lightweight JSONP fetcher
-    //  Copyright 2010-2012 Erik Karlsson. All rights reserved.
-    //  BSD licensed
-  
 
-  
-    //   Usage:
-   
-    // JSONP.get( 'someUrl.php', {param1:'123', param2:'456'}, function(data){
-    //   //do something with data, which is the JSON object you should retrieve from someUrl.php
-    // });
-   
-    jsonP: (function(){
-      var counter = 0, head, window = this, config = {};
-      function load(url, pfnError) {
-        var script = document.createElement('script'),
-          done = false;
-        script.src = url;
-        script.async = true;
+//     //  Lightweight JSONP fetcher
+//     //  Copyright 2010-2012 Erik Karlsson. All rights reserved.
+//     //  BSD licensed
 
-        var errorHandler = pfnError || config.error;
-        if ( typeof errorHandler === 'function' ) {
-          script.onerror = function(ex){
-            errorHandler({url: url, event: ex});
-          };
-        }
 
-        script.onload = script.onreadystatechange = function() {
-          if ( !done && (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete') ) {
-            done = true;
-            script.onload = script.onreadystatechange = null;
-            if ( script && script.parentNode ) {
-              script.parentNode.removeChild( script );
-            }
-          }
-        };
 
-        if ( !head ) {
-          head = document.getElementsByTagName('head')[0];
-        }
-        head.appendChild( script );
-      }
-      function encode(str) {
-        return encodeURIComponent(str);
-      }
-      function jsonp(url, params, callback, callbackName) {
-        var query = (url||'').indexOf('?') === -1 ? '?' : '&', key;
+//     //   Usage:
 
-        callbackName = (callbackName||config['callbackName']||'callback');
-        var uniqueName = callbackName + '_json' + (++counter);
+//     // JSONP.get( 'someUrl.php', {param1:'123', param2:'456'}, function(data){
+//     //   //do something with data, which is the JSON object you should retrieve from someUrl.php
+//     // });
 
-        params = params || {};
-        for ( key in params ) {
-          if ( params.hasOwnProperty(key) ) {
-            query += encode(key) + '=' + encode(params[key]) + '&';
-          }
-        }
+//     jsonP: (function(){
+//       var counter = 0, head, window = this, config = {};
+//       function load(url, pfnError) {
+//         var script = document.createElement('script'),
+//           done = false;
+//         script.src = url;
+//         script.async = true;
 
-        window[ uniqueName ] = function(data){
-          callback(data);
-          try {
-            delete window[ uniqueName ];
-          } catch (e) {}
-          window[ uniqueName ] = null;
-        };
+//         var errorHandler = pfnError || config.error;
+//         if ( typeof errorHandler === 'function' ) {
+//           script.onerror = function(ex){
+//             errorHandler({url: url, event: ex});
+//           };
+//         }
 
-        load(url + query + callbackName + '=' + uniqueName);
-        return uniqueName;
-      }
-      function setDefaults(obj){
-        config = obj;
-      }
-      return {
-        get:jsonp,
-        init:setDefaults
-      };
-    }()),
-    load : function(options) {
-      var callback, month, day, host;
+//         script.onload = script.onreadystatechange = function() {
+//           if ( !done && (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete') ) {
+//             done = true;
+//             script.onload = script.onreadystatechange = null;
+//             if ( script && script.parentNode ) {
+//               script.parentNode.removeChild( script );
+//             }
+//           }
+//         };
 
-      if ( typeof(options) === 'function' ) {
-        callback = options;
-      }
-      else if ( typeof(options) === 'object' ) {
-        callback = options.callback;
-        month = options.month;
-        day = options.day;
-      }
+//         if ( !head ) {
+//           head = document.getElementsByTagName('head')[0];
+//         }
+//         head.appendChild( script );
+//       }
+//       function encode(str) {
+//         return encodeURIComponent(str);
+//       }
+//       function jsonp(url, params, callback, callbackName) {
+//         var query = (url||'').indexOf('?') === -1 ? '?' : '&', key;
 
-      this.jsonP.get(this.host + '/date', {}, function(tmp) {
-        historyData.data = tmp.data;
-        historyData.url = tmp.url;
-        historyData.date = tmp.date;
+//         callbackName = (callbackName||config['callbackName']||'callback');
+//         var uniqueName = callbackName + '_json' + (++counter);
 
-        if ( typeof(callback) === 'function' ) {
-          callback(historyData.data);
-        }
-      });
-    }
-  }
-  module.historyData = historyData;
-})(app)
+//         params = params || {};
+//         for ( key in params ) {
+//           if ( params.hasOwnProperty(key) ) {
+//             query += encode(key) + '=' + encode(params[key]) + '&';
+//           }
+//         }
+
+//         window[ uniqueName ] = function(data){
+//           callback(data);
+//           try {
+//             delete window[ uniqueName ];
+//           } catch (e) {}
+//           window[ uniqueName ] = null;
+//         };
+
+//         load(url + query + callbackName + '=' + uniqueName);
+//         return uniqueName;
+//       }
+//       function setDefaults(obj){
+//         config = obj;
+//       }
+//       return {
+//         get:jsonp,
+//         init:setDefaults
+//       };
+//     }()),
+//     load : function(options) {
+//       var callback, month, day, host;
+
+//       if ( typeof(options) === 'function' ) {
+//         callback = options;
+//       }
+//       else if ( typeof(options) === 'object' ) {
+//         callback = options.callback;
+//         month = options.month;
+//         day = options.day;
+//       }
+
+//       this.jsonP.get(this.host + '/date', {}, function(tmp) {
+//         historyData.data = tmp.data;
+//         historyData.url = tmp.url;
+//         historyData.date = tmp.date;
+
+//         if ( typeof(callback) === 'function' ) {
+//           callback(historyData.data);
+//         }
+//       });
+//     }
+//   }
+//   module.historyData = historyData;
+// })(app)
