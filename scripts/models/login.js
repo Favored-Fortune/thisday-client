529598ee783f62724978b7d9231ae4ab3d2990a2
Steven Starwalt <steven.starwalt@yahoo.com>
2018-03-07 14:27:47 -0800

    added the full data base and fixed an async problem

--- scripts/models/login.js
+++ scripts/models/login.js
@@ -9,31 +9,40 @@ var API_URL = 'http://localhost:3000';
 
   function User (user) {
     this.username = user.username,
-    this.datesearched = user.date_searched
+    this.fav_date = user.fav_date
   }
 
   $('#user-form').on('submit', function(event){
     event.preventDefault();
     let username = event.target.username.value;
-    User.getAll(User.checkUser(username));
+    User.getAll(User.checkUser, username);
   });
 
   User.checkUser = function(user) {
+    console.log(User.all);
+    console.log(user);
     const userCheck = User.all.filter(userObj => userObj.username === user)
+    console.log(userCheck);
     if(userCheck.length === 1) {
       app.requestView.initKnownUser(userCheck);
     }else{
-      app.initRequestView;
+      app.requestView.initNewUser(user);
+      $('#requestDate').on('submit', function(event, user){
+        $.post(`${API_URL}/api/vi/newUser/`)
+      })
     }
   };
 
   User.loadAll = rows => User.all = rows.map(user => new User(user));
 
-  User.getAll = callback =>
+  User.getAll = (callback, username) => {
     $.get(`${API_URL}/api/v1/users`)
-      .then(User.loadAll)
-      .then(callback)
+      .then((results) => {
+        User.loadAll(results)
+        callback(username)
+      })
       .catch(console.error);
+  }
   module.User = User;
 
 })(app)
--- scripts/views/requestView.js
+++ scripts/views/requestView.js
@@ -5,14 +5,26 @@ var app = app || {};
 (module => {
   const requestView = {}
 
+  requestView.initLoginPage = () => {
+    $('.tab-content').hide();
+    $('.login').fadeIn(750);
+  }
+
   requestView.initKnownUser = (user) => {
+    console.log(user);
     $('.tab-content').hide();
     $('.request').fadeIn(750);
-    let date = user[0].datesearched.split('T')[0].split('-');
+    let date = user[0].fav_date.split('T')[0].split('-');
     $('#year').val(date[0]);
     $('#month').val(date[1]);
     $('#day').val(date[2]);
   };
+
+  requestView.initNewUser = (user) => {
+    $('.tab-content').hide();
+    $('.request').fadeIn(750);
+
+  }
 
   module.requestView = requestView;
 })(app)
