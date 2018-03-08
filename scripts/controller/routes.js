fcd6b2580cfa40e2e76dc4c249fd2b5c15a175cc
Suzanne Richman <suzanne@favoredfortune.com>
2018-03-08 14:26:33 -0800

    testing route fix

--- scripts/controller/routes.js
+++ scripts/controller/routes.js
@@ -1,6 +1,7 @@
 'use strict';
 
 page('/', app.requestView.initLoginPage);
+page('/thisday-client', app.requestView.initLoginPage);
 page('/about', app.requestView.initAboutPage);
 page('*', ctx => app.errorView.initErrorPage);
 page();
