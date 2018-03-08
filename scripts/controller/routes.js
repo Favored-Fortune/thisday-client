290178d874d7c441975c494b10f70ef34daa0cef
Suzanne Richman <suzanne@favoredfortune.com>
2018-03-08 15:14:59 -0800

    updating routes function

--- scripts/controller/routes.js
+++ scripts/controller/routes.js
@@ -1,11 +1,10 @@
 'use strict';
 if(window.location.pathname !== '/') {
   page.base('/thisday-client');
+}
 
 page('/', app.requestView.initLoginPage);
 // page('/thisday-client', app.requestView.initLoginPage);
 page('/about', app.requestView.initAboutPage);
 page('*', ctx => app.errorView.initErrorPage);
 page();
-
-
