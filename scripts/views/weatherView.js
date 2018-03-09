dd5e20e3a797dd5670b5dabf803e581ba5093372
Kris Sakarias <ksakarias@gmail.com>
2018-03-08 21:17:20 -0800

    fixed issue with weather image, hid login text on results view

--- scripts/views/weatherView.js
+++ scripts/views/weatherView.js
@@ -4,6 +4,7 @@ var app = app || {};
 
 (module => {
   const weatherView = {}
+  let obj = {};
 
   weatherView.renderWeather = () => {
     //$('.tab-content').hide();
@@ -11,7 +12,7 @@ var app = app || {};
     // jQuery stuff hide/show/etc.
     // call Weather.toHtml to render template
 
-    let obj = {};
+    // let obj = {};
     //temperature is dependent on weather.js
     obj.maxTemp = module.weather.data.TMAX;
     obj.minTemp = module.weather.data.TMIN;
@@ -30,12 +31,13 @@ var app = app || {};
     })()
 
     obj.image = (() => {
+      console.log(obj.weathering);
       let image = 'images/sun-cloud.png';
-      if(module.obj.weathering === 'Snowed'){
+      if(obj.weathering === 'Snowed'){
         image = 'images/snow-cloud.png';
-      } else if(module.obj.weathering === 'Poured'){
+      } else if(obj.weathering === 'Poured'){
         image = 'images/rain-cloud.png';
-      } else if(module.obj.weathering === 'Rained'){
+      } else if(obj.weathering === 'Rained'){
         image = 'images/rain-cloud.png';
       }
       return image;
